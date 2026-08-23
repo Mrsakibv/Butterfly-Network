1c31917f31765bc4535f68d878647aaf0aa5e2a0
Butterfly Network <mrsakib.us@gmail.com>
2026-08-23 20:32:22 +0600

    update website

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,7 +16,7 @@ interface HomePageProps {
 
 export const HomePage: React.FC<HomePageProps> = ({ onOpenPlayModal }) => {
   useEffect(() => {
-    document.title = 'Butterfly Networko | Minecraft Server Network';
+    document.title = 'Butterfly Networkb | Minecraft Server Network';
   }, []);
 
   return (
